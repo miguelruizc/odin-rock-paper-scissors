cf4e2881dc1904543ef6e4ea83eb00c828218f82
miguelruizc <buumchakalaka@gmail.com>
2024-03-23 14:02:33 +0100

    Add playRound() function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getComputerChoice()
 {
     let random = Math.floor(Math.random() * 3) + 1;
-    console.log("Random variable: " + random);
 
     if (random === 1)
     {
@@ -14,5 +13,53 @@ function getComputerChoice()
     else
     {
         return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    // Convert strings to Capitalized
+    let player = playerSelection.toLowerCase();
+    player = player.charAt(0).toUpperCase() + player.slice(1);
+    let computer = computerSelection.toLowerCase();
+    computer = computer.charAt(0).toUpperCase() + computer.slice(1);
+
+    // Check if player wins
+    if 
+    (
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
+    )
+    {
+        let str = "Player wins! " + player + " beats " + computer;
+        return str;
+    }
+    // Check if computer wins
+    else if 
+    (
+        (computer === "Rock" && player === "Scissors") ||
+        (computer === "Scissors" && player === "Paper") ||
+        (computer === "Paper" && player === "Rock")
+    )
+    {
+        let str = "Computer wins! " + computer + " beats " + player;
+        return str;
+    }
+    // Check tie
+    else if 
+    (
+        (computer === "Rock" && player === "Rock") ||
+        (computer === "Scissors" && player === "Scissors") ||
+        (computer === "Paper" && player === "Paper")
+    )
+    {
+        let str = "Tie!";
+        return str;
+    }
+    // Check invalid input
+    else 
+    {
+        return "Invalid input";
     }
 }
