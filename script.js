eef308a31e8ef9fcdc4bc66925369d49133cbb8f
miguelruizc <buumchakalaka@gmail.com>
2024-03-23 13:38:33 +0100

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("I'm a javascript file");
+function getComputerChoice()
+{
+    let random = Math.floor(Math.random() * 3) + 1;
+    console.log("Random variable: " + random);
+
+    if (random === 1)
+    {
+        return "rock";
+    }
+    else if (random === 2)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+}
